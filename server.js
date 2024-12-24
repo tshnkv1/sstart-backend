39d150c05617f6fd674f4a0b74551b58f1b0d71d
@aliaksandr.tsyhankou <aliaksandr.tsyhankou@britenet.com.pl>
2024-12-24 18:51:06 +0100

    migrate BE part

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/auth');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Подключение к MongoDB
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.error('MongoDB connection error:', err));
+
+// Простой маршрут
+app.use('/api/auth', authRoutes);
+
+app.get('/', (req, res) => {
+  res.send('API is running');
+});
+
+// Запуск сервера
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
